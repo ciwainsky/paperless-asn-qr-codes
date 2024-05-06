df5fc7843c9d0d9c36b1cef7d982f662a77151d9
ciwainsky <christian.iwainsky@hpc-hessen.de>
2024-05-06 15:51:25 +0200

    my little changes to make live easier

--- paperless_asn_qr_codes/main.py
+++ paperless_asn_qr_codes/main.py
@@ -13,11 +13,24 @@ def render(c, x, y):
     barcode_value = f"ASN{startASN:0{digits}d}"
     startASN = startASN + 1
     #print ("Rendering for label-size ",x," ",y)
-    qr = QRCodeImage(barcode_value, size=y * 0.9, border=1.5)
+    qr = QRCodeImage(barcode_value, size=y * 0.9, border=2)
     qr.drawOn(c, .5 * mm, y * 0.05)
     c.setFont("Helvetica", 2.9 * mm)
     c.drawString(y, (y - 2 * mm) / 2, barcode_value)
 
+def render_special(c, x, y):
+    global startASN
+    global digits
+    barcode_value = f"ASN{startASN:04d}"
+    #print ("Rendering for label-size ",x," ",y)
+    qr = QRCodeImage(barcode_value, size=y * 0.9, border=2)
+    qr.drawOn(c, .5 * mm, y * 0.05)
+    c.setFont("Helvetica", 2.9 * mm)
+    barcode_value = f"ASN"
+    c.drawString(y, (y - 2 * mm) * 3 / 4, barcode_value)
+    barcode_value = f"{startASN:05}"
+    c.drawString(y, (y - 2 * mm) * 1 / 4, barcode_value)
+    startASN = startASN + 1
 
 def main():
     parser = argparse.ArgumentParser(
@@ -38,10 +51,25 @@ def main():
         action="store_true",
         help="Display borders around labels, useful for debugging the printer alignment",
     )
+    parser.add_argument(
+        "--pages",
+        "-p",
+        default=1,
+        help="Generate p-Pages of codes", type=int
+    )
+    
+    parser.add_argument(
+        "--twoLine",
+        "-t",
+        default = False,
+        action="store_true",
+        help="Use special format for ASN",
+    )
     args = parser.parse_args()
     global startASN
     global digits
-    print ("Doing s.th.")
+    global specialASNPrefix
+    print ("Printing labels for ",args.format," starting with ",args.start_asn," with ",args.digits," to ",args.output_file)
     startASN = int(args.start_asn)
     digits = int(args.digits)
     label = avery_labels.AveryLabel(args.format, args.border)
@@ -50,8 +78,11 @@ def main():
     count = (
         avery_labels.labelInfo[args.format].labels_horizontal
         * avery_labels.labelInfo[args.format].labels_vertical
-    )
-    label.render(render, count)
+    )*args.pages
+    if bool(args.twoLine):
+        label.render(render_special,count)
+    else:
+        label.render(render, count)
     label.close()
 
 
